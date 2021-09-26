52e8f0a227ab6db3d3f59b6968b25216ffa7c883
Quany <quany@live.com>
2021-09-26 15:01:22 +0800

    spec

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Mp from 'react-native-mp';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
